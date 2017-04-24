775f348114535f45d8924c6b166119ec4c39abbb
Illyoung Choi <iychoi@email.arizona.edu>
2017-04-24 16:23:24 -0700

    Make docker image more compact

--- containers/install_r_packages.r
+++ containers/install_r_packages.r
@@ -1,10 +1,10 @@
 #!/usr/bin/env Rscript
 
-install.packages("optparse")
-install.packages("vegan")
-install.packages("igraph")
-install.packages("networkD3")
-install.packages("R.utils")
-install.packages("xtable")
-install.packages("hash")
-install.packages("qgraph")
+#install.packages("optparse")
+#install.packages("vegan")
+#install.packages("igraph")
+#install.packages("networkD3")
+#install.packages("R.utils")
+#install.packages("xtable")
+#install.packages("hash")
+#install.packages("qgraph")
