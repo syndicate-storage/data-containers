0423548e83f58436f2d2c30f522f7dd75fe3c0ed
Illyoung Choi <iychoi@email.arizona.edu>
2017-06-06 21:45:55 -0700

    Refine comments/prints

--- syndicate_base/mount_syndicatefs.sh
+++ syndicate_base/mount_syndicatefs.sh
@@ -20,21 +20,18 @@ syndicate -d reload_gateway_cert ${READER_UG_NAME}
 echo "Registering Syndicate... Done!"
 
 
-# RUN UG for read
-echo "Run UG for read..."
+# MOUNT ANONYMOUS UG
+echo "Mounting an anonymous UG..."
 sudo mkdir -p ${SYNDICATEFS_DATASET_MOUNT_DIR}
 sudo chown -R syndicate:syndicate ${SYNDICATEFS_DATASET_MOUNT_DIR}
 sudo chmod -R 744 ${SYNDICATEFS_DATASET_MOUNT_DIR}
 
 syndicatefs -f -u ANONYMOUS -v ${DATASET_VOLUME} -g ${READER_UG_NAME} -d3 ${SYNDICATEFS_DATASET_MOUNT_DIR} &> /tmp/syndicate_ug_read.log&
 waitfusemount.py syndicatefs ${SYNDICATEFS_DATASET_MOUNT_DIR} 30
-if [ $? -eq 0 ]
-then
-    echo "UG for read is running..."
-else
-    echo "UG for read is not running... Failed"
+if [ $? -ne 0 ]; then
+    echo "Mounting an anonymous UG... Failed"
     exit 1
 fi
-echo "Run UG for read... Done!"
+echo "Mounting an anonymous UG... Done!"
 
 /bin/bash
